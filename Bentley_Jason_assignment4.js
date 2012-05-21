cf6c9e0028e1304df53a5295b5ee5b95ee5cc2f7
Jason Bentley <jbxfade@fullsail.edu>
2012-05-21 11:19:36 -0700

    figured our revealing module and coded it for my library: jasLib

--- Bentley_Jason_assignment4.js
+++ Bentley_Jason_assignment4.js
@@ -5,17 +5,45 @@
 // Created for: SDI Online 1205
 // First assignment: Project 4: Library
 
-
+var jasLib = (function() {
 	var phoneValidate = function (phoneString) {
-	 	var filter =  /^((\+?1-)?\d\d\d-)?\d\d\d-\d\d\d\d$/;
-	 	if (filter.test(phoneString)) {
+	 	var phoneRE =  /^((\+?1-)?\d\d\d-)?\d\d\d-\d\d\d\d$/;
+	 	if (phoneRE.test(phoneString)) {
 	 	return true;
 		 } else {
 	 	return false;
-		}
-	 	
-};
+		}		
+	};
+	// var emailValidate =function (emailString) {
+	// 	var emailRE = 
+	// };
+
+	return {
+			phoneValidate: phoneValidate
+	};	
+}());
+
+var jlib = jasLib;
+console.log(jasLib.phoneValidate("333-444-4444"));
+console.log(jlib.phoneValidate("333-324-2323"));
 
 
-var checkPhone = phoneValidate("317-298-9930");
-console.log(checkPhone);
+
+
+
+
+
+
+
+
+
+
+
+
+// 	var emailValidate = function (emailString) {
+// 		var emailRE
+// 	}
+
+
+// var checkPhone = phoneValidate("317-298-9930");
+// console.log(checkPhone);
