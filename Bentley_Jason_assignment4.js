323acbaaabf466bd353e0df6b29565a9ca52fe6d
Jason Bentley <jbxfade@fullsail.edu>
2012-05-21 12:57:29 -0700

    coded url validator

--- Bentley_Jason_assignment4.js
+++ Bentley_Jason_assignment4.js
@@ -22,10 +22,19 @@ var jasLib = (function() {
 			return false;
 		}
 	};
+	var urlValidate = function (urlString) {
+		var urlRE = /^(ht|f)tps?:\/\/[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s&lt;&gt;\#%"\,\{\}\\|\\\^\[\]`]+)?$/;
+		if (urlRE.test(urlString)) {
+			return true;
+		} else {
+			return false;
+		}
+	};
 
 	return {
 			phoneValidate: phoneValidate,
-			emailValidate: emailValidate
+			emailValidate: emailValidate,
+			urlValidate: urlValidate
 	};	
 }());
 
@@ -33,6 +42,7 @@ var jlib = jasLib;
 console.log(jasLib.phoneValidate("333-444-4444"));
 console.log(jlib.phoneValidate("333-324-2323"));
 console.log(jlib.emailValidate("jbae@fullsail.edu"));
+console.log(jlib.urlValidate("https://www.this.com"));
 
 
 
